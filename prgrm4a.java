805149f9bd2e1ad50253a70840e1f26517866499
katerinachinnappan <katiachinnap@gmail.com>
2016-01-31 12:11:22 -0800

    Create prgrm4a.java

---
+++ prgrm4a.java
@@ -0,0 +1,14 @@
+//Katerina Chinnappan
+//CIS 35A, Section 61Y
+//Assignment #2
+//Due: 1/20/2016
+//Handed: 1/18/2016
+
+public class TestCoin {
+
+	public static void main(String[] args) {
+		Coin coin = new Coin();
+        
+		coin.tossCoin();
+    }
+}
